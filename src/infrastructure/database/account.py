65f668b604a55e603997cca478786590e796de9f
Hanishka-Kela <hanishkaappl@gmail.com>
2026-08-15 22:37:13 +0530

    Add relationship mapping between Account and User models

--- src/infrastructure/database/account.py
+++ src/infrastructure/database/account.py
@@ -1,8 +1,13 @@
 from src.infrastructure.database.base import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 from sqlalchemy import UUID, String, ForeignKey,Enum
 import uuid
 from src.domain.account import AccountType
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.infrastructure.database.user import User
+
 
 class Account(Base):
     __tablename__ = "accounts"
@@ -10,3 +15,4 @@ class Account(Base):
     owner_id :Mapped[uuid.UUID]= mapped_column(UUID,ForeignKey("users.user_id"))
     name:Mapped[str]=mapped_column(String, nullable=False)
     type: Mapped[AccountType] = mapped_column(Enum(AccountType))
+    owner:Mapped["User"] = relationship(back_populates="accounts")
--- src/infrastructure/database/user.py
+++ src/infrastructure/database/user.py
@@ -1,10 +1,18 @@
 from src.infrastructure.database.base import Base
 import uuid
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import String,UUID
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.infrastructure.database.account import Account
+
 
 class User(Base):
     __tablename__ = "users"
     user_id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key = True)
     email: Mapped[str]= mapped_column(String, unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String, nullable=False)
+    accounts: Mapped[list["Account"]] = relationship(
+        back_populates="owner"
+    )
