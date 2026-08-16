778ef26b40daebb8774c14fde06b1af9c6d159b4
Hanishka-Kela <hanishkaappl@gmail.com>
2026-08-16 21:34:58 +0530

    Enable foreign key support in SQLite by adding event listener

--- src/infrastructure/database/engine.py
+++ src/infrastructure/database/engine.py
@@ -1,3 +1,10 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 
 engine = create_engine("sqlite:///ledger.db")
+
+
+@event.listens_for(engine, "connect")
+def enable_sqlite_foreign_keys(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
