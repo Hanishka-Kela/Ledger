5b28af0b29d5ad31439e95761bfc2bca489f3022
Hanishka-Kela <hanishkaappl@gmail.com>
2026-08-23 20:12:42 +0530

    Add create_account method to AccountService for account creation

--- src/application/account_service.py
+++ src/application/account_service.py
@@ -2,7 +2,8 @@ from src.infrastructure.repositories.user_repository import UserRepository
 from src.infrastructure.repositories.account_repository import AccountRepository
 from src.domain.user import User as DomainUser
 from src.domain.account import Account as DomainAccount
-from uuid import UUID
+from src.domain.account import AccountType
+from uuid import UUID, uuid4
 
 class AccountService:
 
@@ -16,3 +17,17 @@ class AccountService:
             raise ValueError("User not Found")
         
         return self.account_repository.get_by_owner_id(user_id)
+
+    def create_account(self,user_id:UUID, name:str, account_type:AccountType) -> DomainAccount:
+        user = self.user_repository.get_by_id(user_id)
+        if user is None: 
+            raise ValueError("User not found")
+
+        account = DomainAccount(
+            account_id=uuid4(),
+            owner_id=user_id,
+            name=name,
+            type=account_type,
+        )
+
+        return self.account_repository.create(account)
