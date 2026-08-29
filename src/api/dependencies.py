d57affe1a39987b6c3566a776b43f63da591fb68
Hanishka-Kela <hanishkaappl@gmail.com>
2026-08-29 18:25:45 +0530

    Add user authentication dependency and current user retrieval function

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,4 +1,13 @@
 from src.infrastructure.database.session import SessionLocal
+from fastapi.security import OAuth2PasswordBearer
+from src.infrastructure.security.jwt import verify_access_token
+from src.infrastructure.repositories.user_repository import UserRepository
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl = "/auth/login"
+)
 
 def get_db():
     db = SessionLocal()
@@ -7,3 +16,23 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def get_current_user(token:str=Depends(oauth2_scheme), db:Session=Depends(get_db)):
+
+    try:
+        user_id = verify_access_token(token)
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials")
+
+    user_repository = UserRepository(db)
+    user = user_repository.get_by_id(user_id)
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials"
+        )
+
+    return user
---
+++ src/infrastructure/database/__init__.py
@@ -0,0 +1,4 @@
+from src.infrastructure.database.user import User
+from src.infrastructure.database.account import Account
+from src.infrastructure.database.transaction import Transaction
+from src.infrastructure.database.entry import Entry
