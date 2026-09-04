28e21e78ac804a389e199094501097e8da9bfb29
Hanishka-Kela <hanishkaappl@gmail.com>
2026-09-04 15:52:18 +0530

    Format code in transaction.py for improved readability

--- src/api/schemas/transaction.py
+++ src/api/schemas/transaction.py
@@ -1,26 +1,34 @@
 from pydantic import BaseModel
 from uuid import UUID
 from datetime import datetime
+
 from src.domain.entry import EntryType
 
+
 class CreateTransactionRequest(BaseModel):
-    source_account_id:UUID
-    destination_account_id:UUID
-    amount:int 
-    description:str
+    source_account_id: UUID
+    destination_account_id: UUID
+    amount: int
+    description: str
+
+
+class CreateOpeningBalanceRequest(BaseModel):
+    account_id: UUID
+    equity_account_id: UUID
+    amount: int
+    description: str
 
 
 class EntryResponse(BaseModel):
-    entry_id:UUID
-    transaction_id:UUID
-    account_id:UUID
-    type:EntryType
-    amount:int
+    entry_id: UUID
+    transaction_id: UUID
+    account_id: UUID
+    type: EntryType
+    amount: int
 
 
 class TransactionResponse(BaseModel):
-    transaction_id:UUID
-    timestamp:datetime
-    description:str
-    entries:list[EntryResponse]
-
+    transaction_id: UUID
+    timestamp: datetime
+    description: str
+    entries: list[EntryResponse]
